5c7a5cdb6a2798c3761887e614fc404a17d71940
Mateo <mateoghiano10@gmail.com>
2025-03-27 01:33:46 -0300

    welcome page

--- delete.php
+++ delete.php
@@ -4,9 +4,8 @@ require "database.php";
 
 $id = $_GET["id"];
 
-
 $statement = $conn->prepare("SELECT * FROM contacts WHERE id = :id");
-$statement->execute([":id" =>$id]);
+$statement->execute([":id" => $id]);
 
 if ($statement->rowCount() == 0) {
   http_response_code(404);
@@ -14,7 +13,6 @@ if ($statement->rowCount() == 0) {
   return;
 }
 
-$statement = $conn->prepare("DELETE FROM contacts WHERE id = :id")->execute([":id" =>$id]);
+$conn->prepare("DELETE FROM contacts WHERE id = :id")->execute([":id" => $id]);
 
-
-header("Location: index.php");
+header("Location: home.php");
