1d032ad5db104b20119cf6a4294d262e5366ca91
Mateo <mateoghiano10@gmail.com>
2025-03-27 15:46:29 -0300

    Create user

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,15 @@
-DROP DATABASE contacts_app;
+DROP DATABASE IF EXISTS contacts_app;
 
 CREATE DATABASE contacts_app;
 
 USE contacts_app;
+
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255)
+);
 
 CREATE TABLE contacts (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -11,3 +18,4 @@ CREATE TABLE contacts (
 );
 
 INSERT INTO contacts (name, phone_number) VALUES ("PEPE", "35453");
+
