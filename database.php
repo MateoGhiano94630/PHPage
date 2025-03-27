8c8e2cf923bc87cab6e3ff3ad06dee03d7043a95
Mateo <mateoghiano10@gmail.com>
2025-03-27 00:27:43 -0300

    update finished

--- database.php
+++ database.php
@@ -5,8 +5,12 @@ $database = "contacts_app";
 $user = "root";
 $password = "";
 
-try{
+try {
   $conn = new PDO("mysql:host=$host;dbname=$database", $user, $password);
-} catch(PDOException $e) {
-  die("PDO Connection Error: " . $e -> getMessage());
+  // foreach ($conn->query("SHOW DATABASES") as $row) {
+  //   print_r($row);
+  // }
+  // die();
+} catch (PDOException $e) {
+  die("PDO Connection Error: " . $e->getMessage());
 }
